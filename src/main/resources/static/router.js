ef61018a5fa33ae9208153f0ba721ae3afc300ab
Emanuel Harling <emanuelharling@hotmail.com>
2020-03-26 22:05:28 +0100

    e

--- src/main/resources/static/router.js
+++ src/main/resources/static/router.js
@@ -37,7 +37,7 @@ export const router = new VueRouter({
       },
       {
         name:"entryPoint",
-        path: '/',
+        path: '/e',
         component: home,
         async beforeEnter(to, from, next){
           checkToken(to, from, next)
