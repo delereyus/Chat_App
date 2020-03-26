e98e4c707efe77266b58681237e2e8e919f60f2e
Emanuel Harling <emanuelharling@hotmail.com>
2020-03-26 21:23:40 +0100

    stuff

--- src/main/java/com/example/demo/configs/MyWebMvcConfigurer.java
+++ src/main/java/com/example/demo/configs/MyWebMvcConfigurer.java
@@ -1,17 +1,13 @@
 package com.example.demo.configs;
 
-import org.springframework.boot.jdbc.DataSourceBuilder;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.http.CacheControl;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.resource.PathResourceResolver;
 
-import javax.sql.DataSource;
 import java.io.IOException;
 
 @Configuration
@@ -29,17 +25,10 @@ class MyWebMvcConfigurer implements WebMvcConfigurer {
                                                    Resource location) throws IOException {
                         Resource requestedResource = location.createRelative(resourcePath);
                         return requestedResource.exists() && requestedResource.isReadable() ? requestedResource
-                                : new ClassPathResource("/static/Index.html");
+                                : new ClassPathResource("/static/index.html");
                     }
                 });
     }
 
-    /*@Bean
-    public DataSource dataSource() {
-        DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
-        dataSourceBuilder.driverClassName("org.sqlite.JDBC");
-        dataSourceBuilder.url("jdbc:sqlite:chat-app.db");
-        return dataSourceBuilder.build();
-    }*/
 }
 
